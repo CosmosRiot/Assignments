6dbc7142163a5e952d84742acee40a5daac54ba2
Raymond Crisostomo <rayjguam@gmail.com>
2021-04-18 05:01:21 -0700

    refreshed venvs, started django project

--- python_stack/my_environments/py3Env/Scripts/activate.bat
+++ python_stack/my_environments/py3Env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rayra\documents\Coding Dojo Assignments\python_stack\my_environments\py3Env
+set VIRTUAL_ENV=C:\Users\rayra\Documents\Coding_Dojo_Assignments\python_stack\my_environments\py3Env
 
 if not defined PROMPT set PROMPT=$P$G
 
