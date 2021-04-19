e8dc43103fd40af6e660ecab3bc3dff1dc5c5f03
Raymond Crisostomo <rayjguam@gmail.com>
2021-04-19 04:03:16 -0700

    completed first django project

--- python_stack/django/django_intro/first_django_project/first_app/urls.py
+++ python_stack/django/django_intro/first_django_project/first_app/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index),
+    path('new', views.new),
+    path('create', views.create),
+    path('<int:my_val>', views.show),
+    path('edit/<int:number>', views.edit),
+    path('<int:my_val>/delete', views.destroy),
 ]
--- python_stack/django/django_intro/first_django_project/first_app/views.py
+++ python_stack/django/django_intro/first_django_project/first_app/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
     return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    response = redirect('/')
+    return response
+
+def show(request, my_val):
+    return HttpResponse("placeholder to display blog number: "+ str(my_val))
+
+def edit(request, number):
+    return HttpResponse("placeholder to edit blog " + str(number))
+
+def destroy(request, my_val):
+    response = redirect('/')
+    return response
